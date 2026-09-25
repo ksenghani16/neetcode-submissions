fba2c434d10f43cd1065afd1555e58ea5aeedb53
ksenghani16 <ksenghani16@gmail.com>
2026-09-25 16:40:17 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,19 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.cache=collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        value=self.cache.pop(key)
+        self.cache[key]=value
+        return value
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        if len(self.cache)>=self.capacity:
+            evicted_value,evicted_item=self.cache.popitem(last=False)
+        self.cache[key]=value
+        
